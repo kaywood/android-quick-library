bfed4d7caebd7f0dee2889a82029d23d3ddbd491
allthelucky <winfirm@163.com>
2014-06-18 11:44:27 +0800

    update

--- AndroidQuickLibrary/src/com/quick/library/QuickActivity.java
+++ AndroidQuickLibrary/src/com/quick/library/QuickActivity.java
@@ -8,7 +8,7 @@ public class QuickActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_quick);
 	}
 
 }
--- AndroidQuickLibrary/src/com/quick/library/app/MainActivity.java
+++ AndroidQuickLibrary/src/com/quick/library/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.quick.library.app;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
@@ -22,7 +23,18 @@ public class MainActivity extends SlidingActivity {
 		sm.setShadowDrawable(R.drawable.shadow);
 		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
 		sm.setFadeDegree(0.35f);
+		sm.setMode(SlidingMenu.LEFT);
 		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		sm.setBehindWidth(200);
+		sm.setBehindScrollScale(0.333f);
+		
+		getSlidingMenu().showMenu();//toggle//直接调无效果
+		findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				getSlidingMenu().showMenu();
+			}
+		});
 	}
 
 	@Override
